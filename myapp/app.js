5ec2c139b65d0abd9b69b974ba5efa0bcf410e07
jungho3145 <jungho3145@gmail.com>
2021-06-30 18:21:16 +0900

    cors policy

--- myapp/app.js
+++ myapp/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 require("dotenv").config();
 
 const { sequelize } = require("./models");
@@ -36,6 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
